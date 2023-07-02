b7ce0321ba3f7dd869f79e8fce68a63e4535ca54
pavlo.zaporozkyi <pavlo.zaporozkyi@mev.com>
2023-07-02 17:34:09 +0300

    Add outputs

--- .github/actions/show-data-year-ago/index.js
+++ .github/actions/show-data-year-ago/index.js
@@ -2,10 +2,21 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 
 function index() {
-    core.notice(`Hey ${core.getInput('first-name')} ${core.getInput('first-name')}!`);
+    core.notice(`Hey ${core.getInput('first-name')} ${core.getInput('last-name')}!`);
     core.notice('Hello from show-data-year-ago action!');
 
     exec.exec('ls -la');
+
+    const oneYearAgo = calculateOneYearAgo();
+
+    core.setOutput('one-year-ago', oneYearAgo.toTimeString());
+}
+
+function calculateOneYearAgo() {
+    const today = new Date();
+    const oneYearAgo = new Date(today.getFullYear() - 1, today.getMonth(), today.getDate());
+
+    return oneYearAgo;
 }
 
 index();
