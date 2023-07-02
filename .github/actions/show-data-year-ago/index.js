d815a5ef4f9bd155423244637514d8dcd05a71a7
pavlo.zaporozkyi <pavlo.zaporozkyi@mev.com>
2023-07-02 17:30:42 +0300

    Add inputs

--- .github/actions/show-data-year-ago/index.js
+++ .github/actions/show-data-year-ago/index.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 
 function index() {
+    core.notice(`Hey ${core.getInput('first-name')} ${core.getInput('first-name')}!`);
     core.notice('Hello from show-data-year-ago action!');
 
     exec.exec('ls -la');
