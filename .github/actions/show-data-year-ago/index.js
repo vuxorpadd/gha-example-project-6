55d568d51a5299af935e018f6641d035b12071d9
pavlo.zaporozkyi <pavlo.zaporozkyi@mev.com>
2023-07-02 17:22:05 +0300

    Fix imports?

--- .github/actions/show-data-year-ago/index.js
+++ .github/actions/show-data-year-ago/index.js
@@ -1,4 +1,5 @@
-import core from '@actions/core';
+const core = require('@actions/core');
+const exec = require('@actions/exec');
 
 function index() {
     core.notice('Hello from show-data-year-ago action!');
